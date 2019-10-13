9b376c548eeb453ba08ede5315749835d0fbe95a
NitigonOnnang <nitigon@gmail.com>
2019-10-13 14:27:14 +0700

    Add files via upload

--- User_Setup_Select.h
+++ User_Setup_Select.h
@@ -20,7 +20,7 @@
 // Only ONE line below should be uncommented.  Add extra lines and files as needed.
 
 #include <User_Setup.h>           // Default setup is root library folder
-
+#include <User_Setups/TTGO_T_Display.h>
 //#include <User_Setups/Setup1_ILI9341.h>  // Setup file configured for my ILI9341
 //#include <User_Setups/Setup2_ST7735.h>   // Setup file configured for my ST7735
 //#include <User_Setups/Setup3_ILI9163.h>  // Setup file configured for my ILI9163
@@ -49,7 +49,6 @@
 //#include <User_Setups/Setup22_TTGO_T4.h>           // Setup file for ESP32 and TTGO T4 (BTC) ILI9341 SPI bus TFT
 //#include <User_Setups/Setup23_TTGO_TM.h>           // Setup file for ESP32 and TTGO TM ST7789 SPI bus TFT
 //#include <User_Setups/Setup24_ST7789.h>            // Setup file configured for ST7789 240 x 240
-//#include <User_Setups/Setup25_TTGO_T_Display.h>    // Setup file for ESP32 and TTGO T-Display ST7789V SPI bus TFT
 
 //#include <User_Setups/Setup43_ST7735.h>            // Setup file configured for my ST7735S 80x160
 
@@ -118,8 +117,8 @@
 #elif defined (RM68140_DRIVER)
 	 #include "TFT_Drivers/RM68140_Defines.h"
 	 #define  TFT_DRIVER 0x6814
-#elif defined (XYZZY_DRIVER)  // <<<<<<<<<<<<<<<<<<<<<<<< ADD NEW DRIVER HERE
-     #include "TFT_Drivers/XYZZY_Defines.h"
+#elif defined (TTGO_T_DRIVER)  // <<<<<<<<<<<<<<<<<<<<<<<< ADD NEW DRIVER HERE
+     #include "TFT_Drivers/TTGO_T_Display.h"
      #define  TFT_DRIVER 0x0000
 #else
      #define  TFT_DRIVER 0x0000
